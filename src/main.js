83675bf24115f62ddf41d20b1047f332a0317403
yuntai229 <3344046@gmail.com>
2019-03-18 00:04:38 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./main.css";
+
+class Home extends Component {
+  render() {
+    var data1 = require("json!./data/data1.json");
+    return 0;
+  }
+}
+
+export default Home;
